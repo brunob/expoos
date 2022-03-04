0300429974caf1bf53f315639c323e73e6a1d01d
brunob <bruno@eliaz.fr>
2022-03-04 19:30:20 +0100

    offline : cacher les fichiers audio
    maintenant que offline le permet ref https://git.nursit.net/open/offline/-/merge_requests/2

--- expoos_options.php
+++ expoos_options.php
@@ -15,3 +15,6 @@ $GLOBALS['z_blocs'] = array(
 
 // désactiver l'héritage des logos de rubriques
 define('_LOGO_RUBRIQUE_DESACTIVER_HERITAGE', true);
+
+// indiquer à offline de cacher les fichiers audio présents dans les pages
+define('_OFFLINE_RESSOURCES_TAGS', ['audio' => 'src']);
