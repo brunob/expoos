87ff95a889b15f85075c5aa53a778ad7e752a7c8
brunob <bruno@eliaz.fr>
2022-03-10 17:33:42 +0100

    article expo carto : carte en fullwidth au dessus du media
    on la masque au chargement et un lien permet de la toggle au besoin

---
+++ lang/expoos_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	'carte_afficher' => 'Afficher la carte',
+	'carte_masquer' => 'Masquer la carte'
+
+);
