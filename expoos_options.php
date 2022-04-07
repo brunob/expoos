b55d9fa2f184df7eb90ca94f8498f919b9a6d342
brunob <bruno@eliaz.fr>
2022-04-07 14:44:06 +0200

    fond de carte IGN en attendant qu'il soit dispo dans GIS

--- expoos_options.php
+++ expoos_options.php
@@ -17,3 +17,15 @@ define('_LOGO_RUBRIQUE_DESACTIVER_HERITAGE', true);
 
 // indiquer à offline de cacher les fichiers audio présents dans les pages
 define('_OFFLINE_RESSOURCES_TAGS', ['audio' => 'src', 'object' => 'data']);
+
+$GLOBALS['gis_layers']['geoportail'] = [
+	'nom' => 'Geoportail IGN',
+	'layer' => 'L.tileLayer(
+		"https://wxs.ign.fr/decouverte/geoportail/wmts?&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE=normal&TILEMATRIXSET=PM&FORMAT=image/jpeg&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}",
+		{
+			maxNativeZoom: 19,
+			maxZoom: 21,
+			attribution : "<a href=\'https://www.ign.fr/\'>IGN</a>"
+		}
+	)'
+];
