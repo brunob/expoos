af14325d7cf94b45f12d0a58c5dff51c955331ec
brunob <bruno@eliaz.fr>
2022-04-20 15:34:14 +0200

    carto : ajout du fond de carte du BRGM

--- expoos_options.php
+++ expoos_options.php
@@ -29,3 +29,17 @@ $GLOBALS['gis_layers']['geoportail'] = [
 		}
 	)'
 ];
+
+$GLOBALS['gis_layers']['brgm'] = [
+	'nom' => 'BRGM',
+	'layer' => 'L.tileLayer.wms("https://geoservices.brgm.fr/WMS-C/", {
+		layers: "SCAN_F_GEOL250",
+		format: "png",
+		transparent: true,
+		attribution: "<a href=\'http://infoterre.brgm.fr/geoservices-ogc\'>BRGM</a>",
+		crs: L.CRS.EPSG4326,
+		bounds: L.latLngBounds([41.9671,-6.20495], [51.2917,12.2874]),
+		minZoom: 6,
+		maxZoom: 15
+	})'
+];
