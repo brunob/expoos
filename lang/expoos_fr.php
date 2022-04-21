6ebd7796c97a35b6b6f1f5460c2f2cc78a87aa52
brunob <bruno@eliaz.fr>
2022-04-21 15:30:17 +0200

    article expo carto : il peut y avoir plusieurs points annexes pour certains articles

--- lang/expoos_fr.php
+++ lang/expoos_fr.php
@@ -17,6 +17,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	'titre_annexes' => 'À propos',
 	'titre_point_annexe' => 'Géolocalisation de la carrière',
+	'titre_points_annexes' => 'Géolocalisation des carrières',
 	'titre_ps' => 'En savoir plus',
 
 );
